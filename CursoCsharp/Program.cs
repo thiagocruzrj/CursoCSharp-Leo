399421017862bd3c9133059853e95a7a93203567
Thiago Cruz <thagocruz@gmail.com>
2020-04-01 19:09:13 -0300

    Start prod with console application

---
+++ CursoCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
