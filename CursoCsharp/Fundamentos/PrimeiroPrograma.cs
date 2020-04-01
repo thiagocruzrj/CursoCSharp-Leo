7a27a630fa76775443a0de62e4c52147b02b7314
Thiago Cruz <thagocruz@gmail.com>
2020-04-01 19:34:56 -0300

    Create 2 main classes

---
+++ CursoCsharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CursoCsharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+        static void Main()
+        {
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou");
+        }
+    }
+}
