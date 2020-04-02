bb798ca1972debcbe029b7d815d987a770d14ddc
Thiago Cruz <thagocruz@gmail.com>
2020-04-01 23:06:11 -0300

    All integer variables

--- CursoCsharp/Fundamentos/VariaveisEConstantes.cs
+++ CursoCsharp/Fundamentos/VariaveisEConstantes.cs
@@ -18,6 +18,32 @@ namespace CursoCsharp.Fundamentos
             double area = Pi * raio * raio;
             Console.WriteLine(area);
             Console.WriteLine("A area é :" + area);
+
+            // Tipos internos
+
+            bool estaChovendo = false;
+            Console.WriteLine("Está chovendo ?" + estaChovendo);
+
+            byte idade = 45;
+            Console.WriteLine("Idade " + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salario " + salario);
+
+            int menorValorInt = int.MinValue;
+            Console.WriteLine("Menor do int: " + menorValorInt);
+
+            uint populacaoBrasileira = 207_600_000;
+            Console.WriteLine("Polulação brasileira = " + populacaoBrasileira);
+
+            long menorValorLong = long.MinValue;
+            Console.WriteLine("Menor long : " + menorValorLong);
+
+            ulong populacaoMundial = 7_600_000_000;
+            Console.WriteLine("População mundial : " + populacaoMundial);
         }
     }
 }
