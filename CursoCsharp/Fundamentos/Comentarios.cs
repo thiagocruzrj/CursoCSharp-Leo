61c3ea83dc3fffa0701a724442903bb37149ce26
Thiago Cruz <thagocruz@gmail.com>
2020-04-01 22:32:23 -0300

    Learning about comments

---
+++ CursoCsharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CursoCsharp.Fundamentos
+{
+    class Comentarios
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Executar()
+        {
+            // Cuidado com os comentarios desnecessários
+            Console.WriteLine("Códico claor é sempre melhor!");
+            /*
+             * Comentario de multiplas linhas
+             * 
+             */
+            Console.WriteLine("O C# também tem o XML Comments!");
+
+            //Console.WriteLine("1");
+            //Console.WriteLine("2");
+        }
+    }
+}
--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -10,6 +10,7 @@ namespace CursoCSharp
         {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentarios - Fundamentos", Comentarios.Executar}
             });
 
             central.SelecionarEExecutar();
