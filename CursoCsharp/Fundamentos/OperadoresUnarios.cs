511cc4541b07402cf6076a8f6d23b878c82a3572
Thiago Cruz <thagocruz@gmail.com>
2020-04-02 23:17:29 -0300

    Finish UnaryOperators

--- CursoCsharp/Fundamentos/OperadoresUnarios.cs
+++ CursoCsharp/Fundamentos/OperadoresUnarios.cs
@@ -6,7 +6,28 @@ namespace CursoCsharp.Fundamentos
     {
         public static void Executar()
         {
+            var valorNegativo = -5;
+            var num1 = 2;
+            var num2 = 3;
+            var booleano = true;
 
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine(!booleano);
+
+            // pós-fixado
+            num1++;
+            Console.WriteLine(num1);
+
+            // pré-fixado
+            --num1;
+            Console.WriteLine(num1);
+
+            /* num1 será incrementado depois da comparação e num2 será incrementado antes da comparação
+            *  Codigo de difícil leitura
+            */
+            Console.WriteLine(num1++ == --num2);
+
+            Console.WriteLine($"{num1} e {num2}");
         }
     }
 }
