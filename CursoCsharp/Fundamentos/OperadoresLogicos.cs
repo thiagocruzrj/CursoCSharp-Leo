13112522fbeef9019441fbc412855be68b6b94c3
Thiago Cruz <thagocruz@gmail.com>
2020-04-02 22:38:24 -0300

    Testing unary en binary operators

--- CursoCsharp/Fundamentos/OperadoresLogicos.cs
+++ CursoCsharp/Fundamentos/OperadoresLogicos.cs
@@ -6,7 +6,19 @@ namespace CursoCsharp.Fundamentos
     {
         public static void Executar()
         {
+            var executouTrabalho1 = true;
+            var executouTrabalho2 = false;
 
+            var comprouTv50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine("Comprou a Tv 50? {0}", comprouTv50);
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine("Comprou sorvete? {0}", comprouSorvete);
+
+            var comprouTv32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine("Comprou Tv de 32 ? {0}", comprouTv32);
+
+            Console.WriteLine("Mais saudavel ? {0}", !comprouSorvete);
         }
     }
 }
