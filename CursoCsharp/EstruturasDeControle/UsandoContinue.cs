d84ea3cc98be53bf31cdf27a1e3bcff29e297cbe
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 23:59:24 -0300

    Stupid way of doing it

--- CursoCsharp/EstruturasDeControle/UsandoContinue.cs
+++ CursoCsharp/EstruturasDeControle/UsandoContinue.cs
@@ -6,7 +6,17 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            int intevalo = 50;
+            Console.WriteLine($"Numeros pares entre 1 e {intevalo}!");
 
+            for (int i = 1; i < intevalo; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    continue;
+                }
+                Console.Write($"{i} ");
+            }
         }
     }
 }
