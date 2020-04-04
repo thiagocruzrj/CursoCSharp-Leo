0e09f257994e5a9fa5dfc8877d244ee89fb5e4c8
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 22:07:15 -0300

    Best way

--- CursoCsharp/EstruturasDeControle/EstruturaIfElseIf.cs
+++ CursoCsharp/EstruturasDeControle/EstruturaIfElseIf.cs
@@ -10,20 +10,46 @@ namespace CursoCsharp.EstruturasDeControle
             string entrada = Console.ReadLine();
             double.TryParse(entrada, out double nota);
 
-            if(nota >= 9)
+            //if (nota >= 9)
+            //{
+            //    Console.WriteLine("Aluno no quadro de honra");
+            //}
+            //else if (nota >= 7)
+            //{
+            //    Console.WriteLine("Aluno aprovado");
+            //}
+            //else if (nota >= 5)
+            //{
+            //    Console.WriteLine("Recuperaçãoi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Reprovado");
+            //}
+
+            if (nota >= 9)
             {
                 Console.WriteLine("Aluno no quadro de honra");
-            } else if (nota >= 7)
-            {
-                Console.WriteLine("Aluno aprovado");
-            } else if (nota >= 5)
-            {
-                Console.WriteLine("Recuperaçãoi");
-            } else
-            {
-                Console.WriteLine("Reprovado");
             }
+            else
+            {
+                if (nota >= 7)
+                {
+                    Console.WriteLine("Aluno aprovado");
 
+                }
+                else
+                {
+                    if (nota >= 5)
+                    {
+                        Console.WriteLine("Recuperaçãoi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Reprovado");
+                    }
+                }
+            }
             Console.WriteLine("Boas festas");
         }
     }
