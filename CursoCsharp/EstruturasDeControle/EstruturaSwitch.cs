378353b6017afe0661a10820c9419966a0d00e1c
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 22:12:48 -0300

    Create customer service switch

--- CursoCsharp/EstruturasDeControle/EstruturaSwitch.cs
+++ CursoCsharp/EstruturasDeControle/EstruturaSwitch.cs
@@ -6,7 +6,33 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            Console.Write("Avalie o meu atendimento(0-5) ");
+            int.TryParse(Console.ReadLine(), out int nota);
 
+            switch (nota)
+            {
+                case 0:
+                    Console.WriteLine("Péssimo");
+                    break;
+                case 1:
+                case 2:
+                    Console.WriteLine("Ruim");
+                    break;
+                case 3:
+                    Console.WriteLine("Regular");
+                    break;
+                case 4:
+                    Console.WriteLine("Bom");
+                    break;
+                case 5:
+                    Console.WriteLine("Ótimos");
+                    break;
+                default:
+                    Console.WriteLine("Nota inválida");
+                    break;
+            }
+
+            Console.WriteLine("Finalizando atendimento");
         }
     }
 }
