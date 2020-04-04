27f6b0d8380a52eb0f26c616d3ab21132a11c6f4
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 23:53:00 -0300

    Example without break

--- CursoCsharp/EstruturasDeControle/UsandoBreak.cs
+++ CursoCsharp/EstruturasDeControle/UsandoBreak.cs
@@ -6,7 +6,23 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            // Exemplo sem o break
+            Random random = new Random();
+            int num = random.Next(1, 51);
 
+            Console.WriteLine($"O número que queremos é {num} !");
+
+            for (int i = 1; i <= 50 ; i++)
+            {
+                Console.Write($"{i} é o número que quermos ? ");
+                if (i == num)
+                {
+                    Console.WriteLine("Sim!");
+                } else
+                {
+                    Console.WriteLine("Não!");
+                }
+            }
         }
     }
 }
