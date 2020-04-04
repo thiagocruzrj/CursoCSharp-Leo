fd3069f8201552eeb68d086754404744c32b858a
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 21:50:38 -0300

    Finish if struture

--- CursoCsharp/EstruturasDeControle/EstruturaIf.cs
+++ CursoCsharp/EstruturasDeControle/EstruturaIf.cs
@@ -6,7 +6,23 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            bool bomComportamento = false;
+            string entrada;
 
+            Console.Write("Digite a nota do aluno: ");
+            entrada = Console.ReadLine();
+            double.TryParse(entrada, out double nota);
+
+            Console.WriteLine("Possue bom comportamento (S/N)? ");
+            entrada = Console.ReadLine();
+
+            //bomComportamento = entrada == "S" || entrada == "s";
+            bomComportamento = entrada.ToLower() == "s";
+
+            if (nota >= 9.0 && bomComportamento)
+            {
+                Console.WriteLine("Quadro de honra!");
+            }
         }
     }
 }
