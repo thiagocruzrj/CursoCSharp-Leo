ecd50978dc15015d3c7291be83d2f602baf23019
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 23:10:33 -0300

    Finishing dowhile struture

--- CursoCsharp/EstruturasDeControle/EstruturaDoWhile.cs
+++ CursoCsharp/EstruturasDeControle/EstruturaDoWhile.cs
@@ -6,7 +6,17 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            string entrada;
 
+            do
+            {
+                Console.WriteLine("Qual o seu nome ? ");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine($"Seja bem vindo {entrada}");
+                Console.WriteLine("Deseja continuar ? (S/N)");
+                entrada = Console.ReadLine();
+            } while (entrada.ToLower() == "s");
         }
     }
 }
