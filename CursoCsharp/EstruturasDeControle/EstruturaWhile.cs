f0574f07b9d973deb4071008ac153bc4f17d0fe6
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 22:56:39 -0300

    While strategy

--- CursoCsharp/EstruturasDeControle/EstruturaWhile.cs
+++ CursoCsharp/EstruturasDeControle/EstruturaWhile.cs
@@ -6,7 +6,39 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            int palpite = 0;
+            Random random = new Random();
 
+            int numeroSecreto = random.Next(1, 16);
+            bool numeroEncontrado = false;
+            int tentativasRestantes = 5;
+            int tentativas = 0;
+
+            while (tentativasRestantes > 0 && !numeroEncontrado)
+            {
+                Console.Write("Insira um palpite: ");
+                string entrada = Console.ReadLine();
+                int.TryParse(entrada, out palpite);
+
+                tentativas++;
+                tentativasRestantes--;
+
+                if (numeroSecreto == palpite)
+                {
+                    numeroEncontrado = true;
+                    var corAnterior = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Numero encontrado em ({tentativas}) tentativas");
+                    Console.BackgroundColor = corAnterior;
+                } else if (palpite > numeroSecreto) {
+                    Console.WriteLine("Maior que o número secreto ... tente novamente");
+                    Console.WriteLine($"Tentativas restante: {tentativasRestantes}");
+                } else
+                {
+                    Console.WriteLine("Menor que o número secreto ... tente novamente");
+                    Console.WriteLine($"Tentativas restante: {tentativasRestantes}");
+                }
+            }
         }
     }
 }
