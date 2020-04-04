49ffdf6c369ed39106f2995c42d9e0f33bf06af3
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 23:12:29 -0300

    Add for class struture on program class

--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -33,6 +33,7 @@ namespace CursoCSharp
                 {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
                 {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
                 {"Estrutura DoWhile - Estruturas de Controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
             });
 
             central.SelecionarEExecutar();
