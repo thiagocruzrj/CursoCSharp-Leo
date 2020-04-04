a629748ad181ed1901fbea944e6fb2011f5fdea8
Thiago Cruz <thagocruz@gmail.com>
2020-04-03 22:01:53 -0300

    Create elseif sample

--- CursoCsharp/EstruturasDeControle/EstruturaIfElseIf.cs
+++ CursoCsharp/EstruturasDeControle/EstruturaIfElseIf.cs
@@ -6,7 +6,25 @@ namespace CursoCsharp.EstruturasDeControle
     {
         public static void Executar()
         {
+            Console.WriteLine("Digite sua nota: ");
+            string entrada = Console.ReadLine();
+            double.TryParse(entrada, out double nota);
 
+            if(nota >= 9)
+            {
+                Console.WriteLine("Aluno no quadro de honra");
+            } else if (nota >= 7)
+            {
+                Console.WriteLine("Aluno aprovado");
+            } else if (nota >= 5)
+            {
+                Console.WriteLine("Recuperaçãoi");
+            } else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+            Console.WriteLine("Boas festas");
         }
     }
 }
