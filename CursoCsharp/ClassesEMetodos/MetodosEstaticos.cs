f2bf10f019ab483b92fe04d20ee879401a3566ea
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 20:15:06 -0300

    Static vs instance object

--- CursoCsharp/ClassesEMetodos/MetodosEstaticos.cs
+++ CursoCsharp/ClassesEMetodos/MetodosEstaticos.cs
@@ -4,11 +4,14 @@ namespace CursoCsharp.ClassesEMetodos
 {
     public class CalculadoraEstatica
     {
+
+        // Metodo de classe ou metodo estatico (somente 1 copia)
         public static int Somar(int a, int b)
         {
             return a + b;
         }
 
+        // Metodo de instancia, pode ser instanciado inumeras vezes
         public static int Multiplicar(int a, int b)
         {
             return a * b;
@@ -21,6 +24,8 @@ namespace CursoCsharp.ClassesEMetodos
         {
             var resultadoSoma = CalculadoraEstatica.Somar(2, 4);
             Console.WriteLine($"Resultado: {resultadoSoma}.");
+
+            Console.WriteLine(CalculadoraEstatica.Multiplicar(10, 7));
         }
     }
 }
