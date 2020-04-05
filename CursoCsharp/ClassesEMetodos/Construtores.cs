4a53bc28a09ccf16d630df11850eab2443582b7f
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 18:32:19 -0300

    Old Constructor removed, because there's no paramiters

--- CursoCsharp/ClassesEMetodos/Construtores.cs
+++ CursoCsharp/ClassesEMetodos/Construtores.cs
@@ -7,18 +7,28 @@ namespace CursoCsharp.ClassesEMetodos
         public string Modelo;
         public string Fabricante;
         public int Ano;
+
+        public Carro(string modelo, string fabricante, int ano)
+        {
+            Modelo = modelo;
+            Fabricante = fabricante;
+            Ano = ano;
+        }
     }
 
     class Construtores
     {
         public static void Executar()
         {
-            Carro carro = new Carro();
-            carro.Modelo = "X1";
-            carro.Fabricante = "BMW";
-            carro.Ano = 2017;
+            //Carro carro = new Carro();
+            //carro.Modelo = "X1";
+            //carro.Fabricante = "BMW";
+            //carro.Ano = 2017;
 
-            Console.WriteLine($"O carro {carro.Modelo} da fabricante {carro.Fabricante} construido no ano {carro.Ano} foi leiloado.");
+            //Console.WriteLine($"O carro {carro.Modelo} da fabricante {carro.Fabricante} construido no ano {carro.Ano} foi leiloado.");
+
+            var carro2 = new Carro("Ka", "Ford", 2018);
+            Console.WriteLine($"O carro {carro2.Modelo} da fabricante {carro2.Fabricante} construido no ano {carro2.Ano} foi leiloado.");
         }
     }
 }
