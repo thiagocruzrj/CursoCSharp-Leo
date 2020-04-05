46c8884d5e6d2634cab10506a6c5cc20b3179cda
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 20:55:07 -0300

    Using params to add how many params do you want

--- CursoCsharp/ClassesEMetodos/ParametrosVariaveis.cs
+++ CursoCsharp/ClassesEMetodos/ParametrosVariaveis.cs
@@ -4,9 +4,17 @@ namespace CursoCsharp.ClassesEMetodos
 {
     class ParametrosVariaveis
     {
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+        }
+
         public static void Executar()
         {
-
+            Recepcionar("Isabella", "Thainá", "Suzi");
         }
     }
 }
