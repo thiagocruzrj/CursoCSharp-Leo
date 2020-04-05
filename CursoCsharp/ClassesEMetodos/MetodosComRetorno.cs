dfc7b8de66e264baad4810b8b9169fea698b9cc9
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 19:33:18 -0300

    Simple calculator

--- CursoCsharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCsharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -2,11 +2,38 @@
 
 namespace CursoCsharp.ClassesEMetodos
 {
+    class CalculadoraComum
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(int a, int b)
+        {
+            return (double)a / (double)b;
+        }
+    }
+
     class MetodosComRetorno
     {
         public static void Executar()
         {
-
+            var calculadoraComum = new CalculadoraComum();
+            Console.WriteLine(calculadoraComum.Somar(1,2));
+            Console.WriteLine(calculadoraComum.Subtrair(1,2));
+            Console.WriteLine(calculadoraComum.Multiplicar(1,2));
+            Console.WriteLine(calculadoraComum.Dividir(1,2));
         }
     }
 }
