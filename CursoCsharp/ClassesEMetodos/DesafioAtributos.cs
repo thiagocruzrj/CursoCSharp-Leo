b3cdf3e71598ef031bdbeaef5bd3e2bcd0008807
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 20:47:54 -0300

    Solving challange

--- CursoCsharp/ClassesEMetodos/DesafioAtributos.cs
+++ CursoCsharp/ClassesEMetodos/DesafioAtributos.cs
@@ -4,9 +4,12 @@ namespace CursoCsharp.ClassesEMetodos
 {
     class DesafioAtributos
     {
+        int a = 10;
+
         public static void Executar()
         {
-
+            DesafioAtributos desafio = new DesafioAtributos();
+            Console.WriteLine(desafio.a);
         }
     }
 }
