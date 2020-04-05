7fd76e69bb46cdbea417a905140fd9fb34ebc828
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 18:34:40 -0300

    Add two ways to construct a object

--- CursoCsharp/ClassesEMetodos/Construtores.cs
+++ CursoCsharp/ClassesEMetodos/Construtores.cs
@@ -14,18 +14,23 @@ namespace CursoCsharp.ClassesEMetodos
             Fabricante = fabricante;
             Ano = ano;
         }
+
+        public Carro()
+        {
+
+        }
     }
 
     class Construtores
     {
         public static void Executar()
         {
-            //Carro carro = new Carro();
-            //carro.Modelo = "X1";
-            //carro.Fabricante = "BMW";
-            //carro.Ano = 2017;
+            var carro = new Carro();
+            carro.Modelo = "X1";
+            carro.Fabricante = "BMW";
+            carro.Ano = 2017;
 
-            //Console.WriteLine($"O carro {carro.Modelo} da fabricante {carro.Fabricante} construido no ano {carro.Ano} foi leiloado.");
+            Console.WriteLine($"O carro {carro.Modelo} da fabricante {carro.Fabricante} construido no ano {carro.Ano} foi leiloado.");
 
             var carro2 = new Carro("Ka", "Ford", 2018);
             Console.WriteLine($"O carro {carro2.Modelo} da fabricante {carro2.Fabricante} construido no ano {carro2.Ano} foi leiloado.");
