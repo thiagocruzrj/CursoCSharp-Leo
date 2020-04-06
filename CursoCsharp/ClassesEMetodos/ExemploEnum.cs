a9fdd8b60bf64c00f0065ffe229ba2dde946ebb8
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 23:15:02 -0300

    Finish enum class

--- CursoCsharp/ClassesEMetodos/ExemploEnum.cs
+++ CursoCsharp/ClassesEMetodos/ExemploEnum.cs
@@ -2,11 +2,27 @@
 
 namespace CursoCsharp.ClassesEMetodos
 {
+    public enum Genero { Ação, Aventura, Terror, SciFi, Comedia };
+
+    public class Filme
+    {
+        public string Titulo;
+        public Genero GeneroDoFilme;
+    }
+
     class ExemploEnum
     {
         public static void Executar()
         {
+            // fazendo cast para converter para inteiro um enum
+            int id = (int)Genero.SciFi;
+            Console.WriteLine(id);
 
+            Filme filmeCasal = new Filme();
+            filmeCasal.Titulo = "Bruxa de Blair";
+            filmeCasal.GeneroDoFilme = Genero.Terror;
+
+            Console.WriteLine($"{filmeCasal.Titulo} é um {filmeCasal.GeneroDoFilme}");
         }
     }
 }
