b80736da1d471714828d9a43f1dea1094dd64196
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 22:46:55 -0300

    Finish class CarroOpcional

--- CursoCsharp/ClassesEMetodos/Propriedades.cs
+++ CursoCsharp/ClassesEMetodos/Propriedades.cs
@@ -2,6 +2,38 @@
 
 namespace CursoCsharp.ClassesEMetodos
 {
+    public class CarroOpcional
+    {
+        double desconto = 0.1;
+        string nome;
+
+        public string Nome
+        {
+            get { return "Opcional: " + nome; }
+            set { nome = value; }
+        }
+
+        // Propriedades autoimplementadas
+        public double Preco { get; set; }
+
+        // Somente leitura
+        public double PrecoComDesconto
+        {
+            get => Preco - (desconto * Preco); // Lambda
+        }
+
+        public CarroOpcional(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public CarroOpcional()
+        {
+
+        }
+    }
+
     class Propriedades
     {
         public static void Executar()
