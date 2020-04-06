41493ddd9db5a765348cf66fc2f76991bd587f34
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 22:06:39 -0300

    Add simple properties

--- CursoCsharp/ClassesEMetodos/GetSet.cs
+++ CursoCsharp/ClassesEMetodos/GetSet.cs
@@ -2,6 +2,20 @@
 
 namespace CursoCsharp.ClassesEMetodos
 {
+    public class Moto
+    {
+        private string Marca;
+        private string Modelo;
+        private int Cilindrada;
+
+        public Moto(string marca, string modelo, int cilindrada)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Cilindrada = cilindrada;
+        }
+    }
+
     class GetSet
     {
         public static void Executar()
