2a36c53d42679c0526a175f11c55dcbb9da942ec
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 23:00:08 -0300

    Finish client class

--- CursoCsharp/ClassesEMetodos/Readonly.cs
+++ CursoCsharp/ClassesEMetodos/Readonly.cs
@@ -2,6 +2,23 @@
 
 namespace CursoCsharp.ClassesEMetodos
 {
+    public class Cliente
+    {
+        public string Nome;
+        DateTime Nascimento;
+
+        public Cliente(string nome, DateTime nascimento)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+        }
+
+        public string GetNascimento()
+        {
+            return string.Format("{0}/{1}/{2}", Nascimento.Day, Nascimento.Month, Nascimento.Year);
+        }
+    }
+
     class Readonly
     {
         public static void Executar()
