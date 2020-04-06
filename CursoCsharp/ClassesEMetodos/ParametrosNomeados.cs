5e330eb052ac629d6b2a1ef1d9a7f1740139fd44
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 21:01:52 -0300

    Clean code tip nomeable params

--- CursoCsharp/ClassesEMetodos/ParametrosNomeados.cs
+++ CursoCsharp/ClassesEMetodos/ParametrosNomeados.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CursoCsharp.ClassesEMetodos
 {
     class ParametrosNomeados
     {
+        public static void Formatar(int dia, int mes, int ano)
+        {
+            Console.WriteLine("{0:D2}/{1:D2}/{2}", dia, mes, ano);
+        }
+
         public static void Executar()
         {
-
+            Formatar(mes: 3, dia: 18, ano: 1994);
         }
     }
 }
