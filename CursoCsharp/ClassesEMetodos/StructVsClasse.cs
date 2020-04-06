58388523a6bca12c80fcd03b981c12e67b6dd2e7
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 23:39:40 -0300

    Struct explanation

--- CursoCsharp/ClassesEMetodos/StructVsClasse.cs
+++ CursoCsharp/ClassesEMetodos/StructVsClasse.cs
@@ -2,11 +2,28 @@
 
 namespace CursoCsharp.ClassesEMetodos
 {
+    public struct SPonto
+    {
+        public int X;
+        public int Y;
+    }
+
+    public class CPonto
+    {
+        public int X;
+        public int Y;
+    }
+
     class StructVsClasse
     {
         public static void Executar()
         {
+            SPonto ponto1 = new SPonto { X = 1, Y = 3 };
+            SPonto copiaPonto1 = ponto1; // Atribuição por Valor e não por referencia. (Struct a passagem sempre é por valor e não por referencia)
+            ponto1.X = 3; 
 
+            Console.WriteLine($"Ponto 1 X: {ponto1.X}");
+            Console.WriteLine($"Copia ponto 1 X: {copiaPonto1.X}");
         }
     }
 }
