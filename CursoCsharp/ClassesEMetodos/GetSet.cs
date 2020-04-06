7e8230309c7a9b20a35165d23b4ae5b0e5e1379e
Thiago Cruz <thagocruz@gmail.com>
2020-04-05 22:08:25 -0300

    Add getters and setters to all properties

--- CursoCsharp/ClassesEMetodos/GetSet.cs
+++ CursoCsharp/ClassesEMetodos/GetSet.cs
@@ -14,6 +14,38 @@ namespace CursoCsharp.ClassesEMetodos
             Modelo = modelo;
             Cilindrada = cilindrada;
         }
+
+        public Moto() { }
+
+        public string GetMarca()
+        {
+            return Marca;
+        }
+
+        public void SetMarca(string marca)
+        {
+            Marca = marca;
+        }
+
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+
+        public void SetModelo(string modelo)
+        {
+            Modelo = modelo;
+        }
+
+        public int GetCilindrada()
+        {
+            return Cilindrada;
+        }
+
+        public void SetCilindrada(int cilindrada)
+        {
+            Cilindrada = cilindrada;
+        }
     }
 
     class GetSet
