78b82066d97e6938a98844b2ff40a587c7fe064a
Thiago Cruz <thagocruz@gmail.com>
2020-04-06 22:57:27 -0300

    Refact usando set

--- CursoCsharp/Colecoes/UsandoSet.cs
+++ CursoCsharp/Colecoes/UsandoSet.cs
@@ -1,24 +1,42 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace CursoCsharp.Colecoes
 {
+    public class ProdutoSet
+    {
+        public string Nome;
+        public double Preco;
+
+        public ProdutoSet(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+    }
     class UsandoSet
     {
         public static void Executar()
         {
-            var arrayList = new ArrayList
+            var livro = new ProdutoSet("Game of Thrones", 49.9);
+            var carrinho = new List<ProdutoSet>();
+            carrinho.Add(livro);
+
+            var combo = new List<ProdutoSet>
             {
-                "Word",
-                3,
-                false
+                new ProdutoSet("Camisa", 30.0),
+                new ProdutoSet("Jogo", 70.0),
+                new ProdutoSet("Vioão", 150.0),
             };
 
-            arrayList.Add(3.14);
+            carrinho.AddRange(combo);
+            Console.WriteLine(carrinho.Count);
+            carrinho.RemoveAt(0);
 
-            foreach (var item in arrayList)
+            foreach (var produto in carrinho)
             {
-                Console.WriteLine($"{item} => {item.GetType()}");
+                Console.Write(carrinho.IndexOf(produto));
+                Console.WriteLine($"{produto.Nome} {produto.Preco}");
             }
         }
     }
