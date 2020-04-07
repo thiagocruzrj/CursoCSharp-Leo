412b124c1be61cc9195f928c2ddac6e0ddd4cb2b
Thiago Cruz <thagocruz@gmail.com>
2020-04-07 00:23:17 -0300

    Add stack example

--- CursoCsharp/Colecoes/UsandoStack.cs
+++ CursoCsharp/Colecoes/UsandoStack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace CursoCsharp.Colecoes
 {
@@ -6,7 +7,27 @@ namespace CursoCsharp.Colecoes
     {
         public static void Executar()
         {
+            var pilha = new Stack();
 
+            pilha.Push(3);
+            pilha.Push("a");
+            pilha.Push('A');
+            pilha.Push(3.79f);
+            pilha.Push(true);
+
+            foreach (var items in pilha)
+            {
+                Console.WriteLine($"{items} ");
+            }
+
+            Console.WriteLine($"\nPilhaPop : {pilha.Pop()}");
+
+            foreach (var item in pilha)
+            {
+                Console.WriteLine($"{item} ");
+            }
+
+            Console.WriteLine($"\nPilhaPeek : {pilha.Peek()}");
         }
     }
 }
