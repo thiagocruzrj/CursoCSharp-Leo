04cd235b423d95c0f5bb9dd5278db9ebee1cf06e
Thiago Cruz <thagocruz@gmail.com>
2020-04-06 22:52:00 -0300

    Verify all array types

--- CursoCsharp/Colecoes/UsandoArrayList.cs
+++ CursoCsharp/Colecoes/UsandoArrayList.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 
 namespace CursoCsharp.Colecoes
 {
@@ -7,7 +7,19 @@ namespace CursoCsharp.Colecoes
     {
         public static void Executar()
         {
+            var arrayList = new ArrayList
+            {
+                "Word",
+                3,
+                false
+            };
 
+            arrayList.Add(3.14);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine($"{item} => {item.GetType()}");
+            }
         }
     }
 }
