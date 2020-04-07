1851063e3ec2dee51ad2c52ed56aa55e9d3be95a
Thiago Cruz <thagocruz@gmail.com>
2020-04-06 22:09:21 -0300

    Simple array example

--- CursoCsharp/Colecoes/UsandoArray.cs
+++ CursoCsharp/Colecoes/UsandoArray.cs
@@ -6,7 +6,17 @@ namespace CursoCsharp.Colecoes
     {
         public static void Executar()
         {
+            string[] alunos = new string[5];
+            alunos[0] = "Thiago";
+            alunos[1] = "da";
+            alunos[2] = "Cruz";
+            alunos[3] = "Justo";
+            alunos[4] = "Stone";
 
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
         }
     }
 }
