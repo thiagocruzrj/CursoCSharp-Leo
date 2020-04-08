dbf3bb3ad12ee993492d4e0c9ddf31d58683ddf9
Thiago Cruz <thagocruz@gmail.com>
2020-04-07 23:18:04 -0300

    Amigo distante access

--- CursoCsharp/OO/Encapsulamento.cs
+++ CursoCsharp/OO/Encapsulamento.cs
@@ -17,6 +17,22 @@ namespace CursoCsharp.OO
         }
     }
 
+    public class AmigoDistante
+    {
+        public readonly SubCelebridade amigo = new SubCelebridade();
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("Amigo distante...");
+            Console.WriteLine(amigo.InfoPublica);
+            //Console.WriteLine(amigo.CorDosOlhos);
+            //Console.WriteLine(amigo.NumeroCelular);
+            //Console.WriteLine(amigo.JeitoDeFalar);
+            //Console.WriteLine(amigo.SegredoDeFamilia);
+            //Console.WriteLine(amigo.UsaMuitoPhotoshop);
+        }
+    }
+
     class Encapsulamento
     {
         public static void Executar()
