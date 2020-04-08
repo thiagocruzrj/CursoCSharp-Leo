010e97a2d7646340379a7a1f9f317f6247c9ed87
Thiago Cruz <thagocruz@gmail.com>
2020-04-07 23:03:02 -0300

    Access in the same class

---
+++ OrientacaoObjeto/SubCelebridade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrientacaoObjeto
+{
+    class SubCelebridade
+    {
+        // Todos
+        public string InfoPublica = "Tenho um Ig";
+
+        // Herança
+        protected string CorDosOlhos = "Verde";
+
+        // Mesmo Projeto (assembly)
+        internal ulong NumeroCelular = 5521999999999;
+
+        // Herança ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso muitas girias";
+
+        // Mesma classe ou herança no mesmo projeto
+        private protected string SegredoDeFamilia = "Bla bla";
+
+        // Private é o padrão
+        private bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("SubCelebridade ...");
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDosOlhos);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoDeFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
