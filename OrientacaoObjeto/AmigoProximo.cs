ec000b049456880e10a22a0b9c9aa43bd16e08bd
Thiago Cruz <thagocruz@gmail.com>
2020-04-07 23:09:57 -0300

    Amigo proximo access

---
+++ OrientacaoObjeto/AmigoProximo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OrientacaoObjeto
+{
+    class AmigoProximo
+    {
+        public readonly SubCelebridade amigo = new SubCelebridade();
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("Amigo próximo ...");
+
+            Console.WriteLine(amigo.InfoPublica);
+            //Console.WriteLine(amigo.CorDosOlhos);
+            Console.WriteLine(amigo.NumeroCelular);
+            Console.WriteLine(amigo.JeitoDeFalar);
+            //Console.WriteLine(amigo.SegredoDeFamilia);
+            //Console.WriteLine(amigo.UsaMuitoPhotoshop);
+        }
+    }
+}
