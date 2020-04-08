93fb7546c6ff4ec04150b460f7a25d9aa3f8866e
Thiago Cruz <thagocruz@gmail.com>
2020-04-07 21:45:34 -0300

    Basis car implementation

--- CursoCsharp/OO/Heranca.cs
+++ CursoCsharp/OO/Heranca.cs
@@ -2,6 +2,45 @@
 
 namespace CursoCsharp.OO
 {
+    public class Carro
+    {
+        protected readonly int VelocidadeMaxima;
+        int VelocidadeAtual;
+
+        public Carro(int velocidadeMaxima)
+        {
+            VelocidadeMaxima = velocidadeMaxima;
+        }
+
+        protected int AlterarVelocidade(int delta)
+        {
+            int novaVelocidade = VelocidadeAtual + delta;
+
+            if (novaVelocidade < 0)
+            {
+                VelocidadeAtual = 0;
+            } else if (novaVelocidade > VelocidadeMaxima)
+            {
+                VelocidadeAtual = VelocidadeMaxima;
+            } else
+            {
+                VelocidadeAtual = novaVelocidade;
+            }
+
+            return VelocidadeAtual;
+        }
+
+        public int Acelerar()
+        {
+            return AlterarVelocidade(5);
+        }
+
+        public int Frear()
+        {
+            return AlterarVelocidade(-5);
+        }
+    }
+
     class Heranca
     {
         public static void Executar()
