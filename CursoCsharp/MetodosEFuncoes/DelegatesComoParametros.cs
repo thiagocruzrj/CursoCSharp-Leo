a7b2fac56d4e85dba02897f3cba6821c0bc0a78b
Thiago Cruz <thagocruz@gmail.com>
2020-04-08 20:43:50 -0300

    Delegate parameter example

--- CursoCsharp/MetodosEFuncoes/DelegatesComoParametros.cs
+++ CursoCsharp/MetodosEFuncoes/DelegatesComoParametros.cs
@@ -4,9 +4,24 @@ namespace CursoCsharp.MetodosEFuncoes
 {
     class DelegatesComoParametros
     {
+        public delegate int Operacao(int x, int y);
+
+        public static int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static string Calculadora(Operacao op, int x, int y)
+        {
+            var resultado = op(x, y);
+            return "Resultado :" + resultado;
+        }
+
         public static void Executar()
         {
-
+            Operacao subtracao = (int x, int y) => x - y;
+            Console.WriteLine(Calculadora(subtracao, 10, 3));
+            Console.WriteLine(Calculadora(Soma, 32, 8));
         }
     }
 }
