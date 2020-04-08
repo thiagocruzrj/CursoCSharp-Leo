be806e992a07a4d9d6549228285474039828ca33
Thiago Cruz <thagocruz@gmail.com>
2020-04-07 22:18:19 -0300

    Implement animal instace with 2 constructors

--- CursoCsharp/OO/ConstrutorThis.cs
+++ CursoCsharp/OO/ConstrutorThis.cs
@@ -2,6 +2,36 @@
 
 namespace CursoCsharp.OO
 {
+    public class Animal
+    {
+        public Animal(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string Nome { get; set; }
+    }
+
+    public class Cachorro : Animal
+    {
+        public double Altura { get; set; }
+
+        public Cachorro(string nome) : base(nome)
+        {
+            Console.WriteLine($"Cachorro {nome} instanciado.");
+        }
+
+        public Cachorro(string nome, double altura) : this(nome)
+        {
+            Altura = altura;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} tem {Altura} de altura!";
+        }
+    }
+
     class ConstrutorThis
     {
         public static void Executar()
