dc847161288873c386dfb70a6c9f4ff1eee4428c
Thiago Cruz <thagocruz@gmail.com>
2020-04-08 01:18:50 -0300

    Good sample of interface

--- CursoCsharp/OO/Interfaces.cs
+++ CursoCsharp/OO/Interfaces.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CursoCsharp.OO
 {
@@ -16,11 +17,50 @@ namespace CursoCsharp.OO
         }
     }
 
+    class Subtracao : OperacaoBinaria
+    {
+        public int Operacao(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class Multiplicacao : OperacaoBinaria
+    {
+        public int Operacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class Calculadora
+    {
+        List<OperacaoBinaria> operacoes = new List<OperacaoBinaria>
+        {
+            new Soma(),
+            new Subtracao(),
+            new Multiplicacao(),
+        };
+
+        public string ExecutarOperacoes(int a, int b)
+        {
+            string resultado = "";
+
+            foreach (var op in operacoes)
+            {
+                resultado += $"Usando {op.GetType().Name} = {op.Operacao(a, b)}\n";
+            }
+
+            return resultado;
+        }
+    }
+
     class Interfaces
     {
         public static void Executar()
         {
-
+            var calc = new Calculadora();
+            Console.WriteLine(calc.ExecutarOperacoes(20, 5)); 
         }
     }
 }
