fb96367825cee9b5e55e3872936febc9857f75a1
Thiago Cruz <thagocruz@gmail.com>
2020-04-08 20:26:57 -0300

    Inverting phrase with delegate

--- CursoCsharp/MetodosEFuncoes/DelegateComFuncoesAnonimas.cs
+++ CursoCsharp/MetodosEFuncoes/DelegateComFuncoesAnonimas.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CursoCsharp.MetodosEFuncoes
 {
     class DelegateComFuncoesAnonimas
     {
+        delegate string StringOperacao(string s);
+
         public static void Executar()
         {
-
+            StringOperacao inverter = delegate (string o)
+            {
+                char[] charArray = o.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            };
+            Console.WriteLine(inverter("Doideira isso ae ..."));
         }
     }
 }
