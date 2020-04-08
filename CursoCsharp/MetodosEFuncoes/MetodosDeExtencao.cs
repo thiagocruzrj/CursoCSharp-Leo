20a0206274c8f91120f30ef215e5294b0db5488b
Thiago Cruz <thagocruz@gmail.com>
2020-04-08 20:50:38 -0300

    ExtensionMethod sample

--- CursoCsharp/MetodosEFuncoes/MetodosDeExtencao.cs
+++ CursoCsharp/MetodosEFuncoes/MetodosDeExtencao.cs
@@ -2,11 +2,28 @@
 
 namespace CursoCsharp.MetodosEFuncoes
 {
+    public static class ExtensoesInteiro
+    {
+        public static int Soma(this int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtracao(this int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
     class MetodosDeExtencao
     {
         public static void Executar()
         {
+            int num = 5;
 
+            Console.WriteLine(num.Soma(3));
+            Console.WriteLine(num.Subtracao(10));
+            Console.WriteLine(2.Subtracao(4));
+            Console.WriteLine(2.Soma(4));
         }
     }
 }
