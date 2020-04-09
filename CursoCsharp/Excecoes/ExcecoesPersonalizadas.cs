fa2fa3b44ee5fc189828f604e4980b74b4759076
Thiago Cruz <thagocruz@gmail.com>
2020-04-08 21:23:37 -0300

    Some types of exception

--- CursoCsharp/Excecoes/ExcecoesPersonalizadas.cs
+++ CursoCsharp/Excecoes/ExcecoesPersonalizadas.cs
@@ -2,6 +2,13 @@
 
 namespace CursoCsharp.Excecoes
 {
+    public class NegativoException : Exception
+    {
+        public NegativoException() { }
+        public NegativoException(string message) : base(message) { }
+        public NegativoException(string message, Exception inner) : base(message, inner) { }
+    }
+
     class ExcecoesPersonalizadas
     {
         public static void Executar()
