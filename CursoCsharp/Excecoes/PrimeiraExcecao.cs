a170faa34c662f0e150455b90266f75c0fa08240
Thiago Cruz <thagocruz@gmail.com>
2020-04-08 21:15:54 -0300

    Generic threatment

--- CursoCsharp/Excecoes/PrimeiraExcecao.cs
+++ CursoCsharp/Excecoes/PrimeiraExcecao.cs
@@ -2,11 +2,44 @@
 
 namespace CursoCsharp.Excecoes
 {
+    public class Conta
+    {
+        double Saldo;
+
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new AggregateException("Saldo insuficiente");
+            }
+
+            Saldo -= valor;
+        }
+    }
     class PrimeiraExcecao
     {
         public static void Executar()
         {
+            Conta conta = new Conta(1_245.45);
 
+            try
+            {
+                conta.Sacar(1600);
+                Console.WriteLine("Saque realizado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thanks bro !");
+            }
         }
     }
 }
