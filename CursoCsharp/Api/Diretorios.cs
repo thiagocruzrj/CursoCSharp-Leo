4cb1adf3d15c90be32ae33b2a02e0c0e934bf88d
Thiago Cruz <thagocruz@gmail.com>
2020-04-11 00:05:34 -0300

    Creation directory time

--- CursoCsharp/Api/Diretorios.cs
+++ CursoCsharp/Api/Diretorios.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CursoCsharp.Api
 {
@@ -6,7 +7,22 @@ namespace CursoCsharp.Api
     {
         public static void Executar()
         {
+            var novoDir = @"~/PastaCsharp".ParseHome();
+            var novoDirDestino = @"~/PastaCsharpDestino".ParseHome();
+            var novoDirProj = @"~/Documents/GitHub/CursoCSharp-Leo".ParseHome();
 
+            if (Directory.Exists(novoDir))
+            {
+                Directory.Delete(novoDir, true);
+            }
+
+            if (Directory.Exists(novoDirDestino))
+            {
+                Directory.Delete(novoDirDestino, true);
+            }
+
+            Directory.CreateDirectory(novoDir);
+            Console.WriteLine(Directory.GetCreationTime(novoDir));
         }
     }
 }
