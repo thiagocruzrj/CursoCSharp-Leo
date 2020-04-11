90307b3d9fcc60aabf0e00a95b58f31d288a690a
Thiago Cruz <thagocruz@gmail.com>
2020-04-10 23:43:52 -0300

    Writing a file

--- CursoCsharp/Api/LendoArquivos.cs
+++ CursoCsharp/Api/LendoArquivos.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.IO;
+
 namespace CursoCsharp.Api
 {
     class LendoArquivos
     {
         public static void Executar()
         {
+            var path = @"~/lendo_arquivos.txt".ParseHome();
 
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine("Produto;Preco;Qtde");
+                    sw.WriteLine("Bic preta;2.10;1");
+                    sw.WriteLine("Borracha;0.99;1");
+                }
+            }
         }
     }
 }
