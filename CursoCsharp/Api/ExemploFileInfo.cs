fe9525a48d4a9a449e91970d5d6f5d76453d83ae
Thiago Cruz <thagocruz@gmail.com>
2020-04-10 23:55:16 -0300

    Update ExemploFileInfo.cs
    Create, copy and move file created

--- CursoCsharp/Api/ExemploFileInfo.cs
+++ CursoCsharp/Api/ExemploFileInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CursoCsharp.Api
 {
@@ -6,7 +7,23 @@ namespace CursoCsharp.Api
     {
         public static void Executar()
         {
+            var caminhoOrigem = @"~/arq_origem.txt".ParseHome();
+            var caminhoDestino = @"~/arq_destino.txt".ParseHome();
+            var caminhoCopia = @"~/arq_copia.txt".ParseHome();
 
+            using(StreamWriter sw = File.CreateText(caminhoOrigem))
+            {
+                sw.WriteLine("Original file");
+            }
+
+            FileInfo origem = new FileInfo(caminhoOrigem);
+            Console.WriteLine(origem.Name);
+            Console.WriteLine(origem.IsReadOnly);
+            Console.WriteLine(origem.FullName);
+            Console.WriteLine(origem.Extension);
+
+            origem.CopyTo(caminhoCopia);
+            origem.MoveTo(caminhoDestino);
         }
     }
 }
