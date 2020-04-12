dfd84153d174c6a4e112075711e02761c0c297d6
Thiago Cruz <thagocruz@gmail.com>
2020-04-11 23:47:33 -0300

    Finish generic class implementation

--- CursoCsharp/TopicosAvancados/ExemploGeneric.cs
+++ CursoCsharp/TopicosAvancados/ExemploGeneric.cs
@@ -2,6 +2,28 @@
 
 namespace CursoCsharp.TopicosAvancados
 {
+    public class Caixa<T>
+    {
+        T valorPrivado;
+        public T Coisa { get; set; }
+
+        public Caixa(T coisa)
+        {
+            Coisa = coisa;
+            valorPrivado = Coisa;
+        }
+
+        public T metodoGenerico(T valor)
+        {
+            return new Random().Next(0, 2) == 0 ? Coisa : valor;
+        }
+
+        public T GetValor()
+        {
+            return valorPrivado;
+        }
+    }
+
     class ExemploGeneric
     {
         public static void Executar()
