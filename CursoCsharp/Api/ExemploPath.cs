57e1c30387fdd5e9f38674a74d8190fede1ed17d
Thiago Cruz <thagocruz@gmail.com>
2020-04-11 21:47:17 -0300

    Finish path sample

--- CursoCsharp/Api/ExemploPath.cs
+++ CursoCsharp/Api/ExemploPath.cs
@@ -7,7 +7,31 @@ namespace CursoCsharp.Api
     {
         public static void Executar()
         {
+            var arquivo = @"~exemplo_path_arquivo.txt".ParseHome();
+            var pasta = @"~exemplo_path_pasta.txt".ParseHome();
 
+            if (!File.Exists(arquivo))
+            {
+                using (StreamWriter sw = File.CreateText(arquivo))
+                {
+                    sw.WriteLine("A new file was created!");
+                }
+            }
+
+            if (!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+
+            Console.WriteLine(Path.GetExtension(arquivo));
+            Console.WriteLine(Path.GetFileName(arquivo));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(arquivo));
+            Console.WriteLine(Path.GetDirectoryName(arquivo));
+            Console.WriteLine(Path.HasExtension(arquivo));
+
+            Console.WriteLine(Path.HasExtension(pasta));
+            Console.WriteLine(Path.GetFullPath(pasta));
+            Console.WriteLine(Path.GetPathRoot(pasta));
         }
     }
 }
