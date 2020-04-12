931ded082477567d77329766816213de9aed07df
Thiago Cruz <thagocruz@gmail.com>
2020-04-11 23:39:43 -0300

    Finsh work with dynamic

--- CursoCsharp/TopicosAvancados/ExemploDynamic.cs
+++ CursoCsharp/TopicosAvancados/ExemploDynamic.cs
@@ -6,7 +6,16 @@ namespace CursoCsharp.TopicosAvancados
     {
         public static void Executar()
         {
+            dynamic meuObjeto = "teste";
+            meuObjeto = 3;
+            meuObjeto++;
+            Console.WriteLine(meuObjeto);
 
+            dynamic aluno = new System.Dynamic.ExpandoObject();
+            aluno.nome = "Maira";
+            aluno.nota = 1.7;
+            aluno.idade = 17;
+            Console.WriteLine($"{aluno.nome} {aluno.nota} {aluno.idade}");
         }
     }
 }
